689836503d0508314cca3bb327d2cf733e0dbde7
boomer001 <57313734+boomer001@users.noreply.github.com>
2023-07-04 12:42:08 +0800

    binggo2

--- src/main/resources/static/js/module/BingChat/BingChating.js
+++ src/main/resources/static/js/module/BingChat/BingChating.js
@@ -13,7 +13,7 @@ export default class BingChating {
      */
     bingChat;
     sendMessageManager;
-   // invocationId = 1;
+    // invocationId = 1;
     // historySendMessage = [];
 
 
@@ -25,10 +25,10 @@ export default class BingChating {
      * @param theChatType {"Creative","Balanced","Precise"} 聊天类型 Precise 或 Balanced 或 Creative
      * @param invocationId {number} 对话id，就是这是第几次对话 可以不传
      * */
-    static create(bingChat, charID, clientId, conversationSignature, theChatType, invocationId){
+    static create(bingChat, charID, clientId, conversationSignature, theChatType, invocationId) {
         let bingChating = new BingChating();
         bingChating.bingChat = bingChat;
-        bingChating.sendMessageManager = new SendMessageManager(bingChat,charID, clientId, conversationSignature,theChatType,invocationId);
+        bingChating.sendMessageManager = new SendMessageManager(bingChat, charID, clientId, conversationSignature, theChatType, invocationId);
         // bingChating.invocationId = bingChating.sendMessageManager.invocationId;
         return bingChating;
     }
@@ -36,7 +36,7 @@ export default class BingChating {
      * @param bingChat {BingChat}对象
      * @param sendMessageManager {SendMessageManager} 对象
      * */
-    static createIn(bingChat, sendMessageManager){
+    static createIn(bingChat, sendMessageManager) {
         let bingChating = new BingChating();
         bingChating.bingChat = bingChat;
         bingChating.sendMessageManager = sendMessageManager;
@@ -61,16 +61,12 @@ export default class BingChating {
         // }
 
         let restsrstUrl;
-        // let location = window.location.origin.indexOf('localhost')? 'https://binggo1.docgpt.top':window.location.origin;
-        let location =  'https://binggo1.docgpt.top' ;
-        // if(window.location.protocol==="chrome-extension:"){
-        //     let re = await nBGGFetch(`${window.location.origin}/sydney/ChatHubUrl`);
-        //     restsrstUrl = await re.text();
-        // }else {
-            let p = new URLSearchParams();
-            p.append("randomAddress",RandomAddress.randomAddress);
-            restsrstUrl = `${location.replace('http','ws')}/sydney/ChatHub?${p.toString()}`;
-        // }
+        let location = 'https://binggo2.docgpt.top';
+
+        let p = new URLSearchParams();
+        p.append("randomAddress", RandomAddress.randomAddress);
+        restsrstUrl = `${location.replace('http', 'ws')}/sydney/ChatHub?${p.toString()}`;
+
         try {
             let chatWebSocket = new WebSocket(restsrstUrl);
             let returnMessage = new ReturnMessage(chatWebSocket, onMessage, this);
@@ -78,26 +74,26 @@ export default class BingChating {
                 this.sendMessageManager.sendShakeHandsJson(chatWebSocket);
             }
             //当连接打开时发送
-            let onopen = async (even)=>{
-                if('{}'===JSON.stringify(even)){
-                    await this.sendMessageManager.sendJson(chatWebSocket,{"type":6});
+            let onopen = async (even) => {
+                if ('{}' === JSON.stringify(even)) {
+                    await this.sendMessageManager.sendJson(chatWebSocket, { "type": 6 });
                     await this.sendMessageManager.sendChatMessage(chatWebSocket, message);
                     returnMessage.outOnMessage(onopen);
                 }
             };
             returnMessage.regOnMessage(onopen);
             //ping回复
-            returnMessage.regOnMessage(async (even)=>{
+            returnMessage.regOnMessage(async (even) => {
                 if (even["type"] === 6) {
-                    await this.sendMessageManager.sendJson(chatWebSocket,{"type":6});
+                    await this.sendMessageManager.sendJson(chatWebSocket, { "type": 6 });
                 }
             });
             return returnMessage;
         } catch (e) {
             console.warn(e);
-            if(e.isNewBingGoGoError){
+            if (e.isNewBingGoGoError) {
                 throw new Error(e.message);
-            }else {
+            } else {
                 throw new Error("无法连接到web服务器，请刷新页面重试:" + e.message);
             }
         }
